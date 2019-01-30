8a9491d93a07436a99112d19faa049c21f9ba68d
Sanya <sanyabindlish@gmail.com>
2019-01-30 20:28:24 +0530

    done

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Test{
+	public static void main(String[] args){
+	    try{
+	        int a[] = new int[5];
+	        a[5]=30/2;
+	        Scanner in = new Scanner(System.in);
+	        int b = in.nextInt();
+	    }
+	    catch(ArithmeticException e){System.out.println("task1 is completed");}
+	    catch(ArrayIndexOutOfBoundsException e){System.out.println("task2 is completed");}
+	    catch(Exception e){System.out.println("common task completed");}
+	    System.out.println("rest of the code....");
+	}
+}
