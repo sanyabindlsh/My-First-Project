8bb2326947838ca93f8c7405c730ca6db28c28b0
Sanya <sanyabindlish@gmail.com>
2019-01-30 13:04:09 +0530

    done

---
+++ resizable_main.java
@@ -0,0 +1,9 @@
+class resizable_main
+{
+	public static void main(String[] args)
+	{
+	ResizableCircle rc=new ResizableCircle(10.0);
+	System.out.println("perimeter"+rc.getperimeter());
+	System.out.println("area"+rc.getarea());
+	}
+}
